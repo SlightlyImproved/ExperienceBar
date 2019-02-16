3eac7fecd87cea5edbb6ed0fb2309a917d39d60a
Arthur <arthur@corenzan.com>
2019-02-16 20:59:21 -0200

    First implementation of visibility control

--- Settings.lua
+++ Settings.lua
@@ -1,1 +1,36 @@
-Settings.lua
+-- Slightly Improved™ Experience Bar
+-- The MIT License © 2016 Arthur Corenzan
+
+local NAMESPACE = "SlightlyImprovedExperienceBar"
+
+local settings = {}
+
+local panel =
+{
+    type = "panel",
+    name = "Slightly Improved™ Experience Bar",
+    displayName = "Slightly Improved™ Experience Bar",
+    author = nil,
+    version = nil,
+}
+
+local options =
+{
+    {
+        type = "dropdown",
+        name = "Visibility",
+        tooltip = "Controls the visibility of the Experience Bar. When set to automatic the Experience Bar will only appear in useful situations like when you level up or in the inventory screen.",
+        choices = {"Automatic", "Always Show"},
+        getFunc = function() return settings.visibility end,
+        setFunc = function(value) settings.visibility = value end,
+        -- requiresReload = true,
+    },
+}
+
+CALLBACK_MANAGER:RegisterCallback(NAMESPACE.."_OnAddOnLoaded", function(savedVars)
+    settings = savedVars
+
+    local LAM = LibStub("LibAddonMenu-2.0")
+    LAM:RegisterAddonPanel(NAMESPACE, panel)
+    LAM:RegisterOptionControls(NAMESPACE, options)
+end)
